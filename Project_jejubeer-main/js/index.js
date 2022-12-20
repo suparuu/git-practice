e28a2cb06707bc17493d7147dd316f04d3da979f
suparuu <kkwoo97@naver.com>
2022-12-20 17:57:12 +0900

    12-20-17:57

--- Project_jejubeer-main/js/index.js
+++ Project_jejubeer-main/js/index.js
@@ -1,10 +1,9 @@
 $("body").prepend("<header>");
 $("body").append("<footer>");
 
-$("header").load("../inc.html header>.top", head);
-$("footer").load("../inc.html footer>div");
+$("header").load("./inc.html header", head);
+$("footer").load("./inc.html footer");
 history.scrollRestoration = "manual";
-
 const Ani01 = document.querySelector(".subtitle01"),
   Ani02 = document.querySelector(".subtitle02"),
   Ani03 = document.querySelector(".subtitle03"),
@@ -22,55 +21,10 @@ function head() {
   });
   localStorage.clear();
 }
-/* const Img02 = document.querySelector(".img02"),//제주라거 글씨 이미지
-  Img03 = document.querySelector(".img03"),//제주라거 맥주 이미지
-  Img04 = document.querySelector(".img04"),//위트에일 글씨 이미지
-  Img05 = document.querySelector(".img05"),//위트에일 맥주 이미지
-  Img06 = document.querySelector(".img06"),//펠롱에일 글씨 이미지
-  Img07 = document.querySelector(".img07"),//펠롱에일 맥주 이미지
-  Img08 = document.querySelector(".img08"),//거멍에일 글씨 이미지
-  Img09 = document.querySelector(".img09"),//거멍에일 맥주 이미지
-  Textbox02 = document.querySelector('.textbox02'),//제주라거 텍스트
-  Textbox03 = document.querySelector('.textbox03'),//위트에일 텍스트
-  Textbox04 = document.querySelector('.textbox04'),//펠롱에일 텍스트
-  Textbox05 = document.querySelector('.textbox05');//거멍에일 텍스트
 
-
-  function ScrollEvent(){
-    let Screen = window.innerHeight ;
-    if(Img02.offsetTop - Screen < window.scrollY){
-      Img02.classList.add('on')
-    }
-    if(Img03.offsetTop - Screen < window.scrollY){
-      Img03.classList.add('on')
-      Textbox02.classList.add('on')
-    }
-    if(Img04.offsetTop - Screen < window.scrollY){
-      Img04.classList.add('on')
-    }
-    if(Img05.offsetTop - Screen < window.scrollY){
-      Img05.classList.add('on')
-      Textbox03.classList.add('on')
-    }
-    if(Img06.offsetTop - Screen < window.scrollY){
-      Img06.classList.add('on')
-    }
-    if(Img07.offsetTop - Screen < window.scrollY){
-      Img07.classList.add('on')
-      Textbox04.classList.add('on')
-    }
-    if(Img08.offsetTop - Screen < window.scrollY){
-      Img08.classList.add('on')
-    }
-    if(Img09.offsetTop - Screen < window.scrollY){
-      Img09.classList.add('on')
-      Textbox05.classList.add('on')
-    }
-    console.log(window.innerHeight)
-  } */
-const Img01 = document.querySelectorAll(".animation01"),//글씨 이미지
-      Img02 = document.querySelectorAll(".animation02"),//맥주 이미지
-      Textbox01 = document.querySelectorAll(".animation03");//텍스트
+const Img01 = document.querySelectorAll(".animation01"), //글씨 이미지
+  Img02 = document.querySelectorAll(".animation02"), //맥주 이미지
+  Textbox01 = document.querySelectorAll(".animation03"); //텍스트
 
 function ScrollEvent() {
   let Screen = window.innerHeight;
@@ -80,13 +34,13 @@ function ScrollEvent() {
     }
   });
   Img02.forEach(function (a, key) {
-    if(Img02[key].offsetTop - Screen < window.scrollY){
-      Img02[key].classList.add('on');
+    if (Img02[key].offsetTop - Screen < window.scrollY) {
+      Img02[key].classList.add("on");
     }
   });
   Textbox01.forEach(function (a, key) {
-    if(Textbox01[key].offsetTop - Screen < window.scrollY){
-      Textbox01[key].classList.add('on');
+    if (Textbox01[key].offsetTop - Screen < window.scrollY) {
+      Textbox01[key].classList.add("on");
     }
   });
 }
--- Project_jejubeer-main/js/supporters.js
+++ Project_jejubeer-main/js/supporters.js
@@ -1,1 +1,86 @@
-history.scrollRestoration = "manual";
+/* const imgAni = document.querySelectorAll(".imgbox01 div"); */
+
+/* 
+
+let num = 0
+function abc(){
+imgAni.forEach(function (){
+    if(num<5){
+        if(num==0){
+            imgAni[num].classList.add('on')
+            num++;
+        if(num==1){
+            imgAni[num].classList.add('on')
+            imgAni[num-1].classList.remove('on')
+            num++;
+            if(num==2){
+                imgAni[num].classList.add('on')
+                imgAni[num-1].classList.remove('on')
+                num++;
+                if(num==3){
+                    imgAni[num].classList.add('on')
+                    imgAni[num-1].classList.remove('on')
+                    num++;
+                    if(num==4){
+                        imgAni[num].classList.add('on')
+                        imgAni[num-1].classList.remove('on')
+                        num++;
+            }
+        }
+    }
+}
+        }
+    else{
+        imgAni[num].classList.add('on')
+    }
+}
+})
+console.log('asdsad')
+}
+abc();
+setInterval(abc,2000); */
+$(".imgbox01 div").not(":first").hide();
+let num = 0,
+  fadeInterval;
+
+fadeInterval = setInterval(function () {
+  $(".imgbox01 div").eq(num).fadeOut(1000);
+  if (num == 4) {
+    num = 0;
+  } else {
+    num++;
+  }
+  $(".imgbox01 div").eq(num).fadeIn(1000);
+}, 2000);
+
+function scrollevent(){
+    const ImgScroll = document.querySelectorAll('.scrollevent');
+    let Screen = window.innerHeight;
+    ImgScroll.forEach(function(a,key){
+        if(ImgScroll[key]-Screen < window.scrollY){
+            ImgScroll[key].classList.add('on')
+        }
+    })
+}
+
+
+/* 애봉아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ */
+
+
+window.addEventListener('scroll',scrollevent)
+console.log()
