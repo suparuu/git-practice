4edbff360ad5e620c66009ef0da7f8cd7a6216e3
suparuu <kkwoo97@naver.com>
2023-01-18 18:17:59 +0900

    2023-01-18-18:17

--- ProjectLoL/lol/src/App.js
+++ ProjectLoL/lol/src/App.js
@@ -1,35 +1,30 @@
 import './App.css';
+import {useState, useRef} from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Top from './component/Top';
+import Jungle from './component/Jungle';
+import Mid from './component/Mid';
+import Bottom from './component/Bottom';
+import Supporter from './component/Supporter';
+import ChampData from './component/ChampData';
+
 
 function App() {
-  fetch("http://ddragon.leagueoflegends.com/cdn/13.1.1/data/ko_KR/champion.json")
-  .then(Response => Response.json())
-  .then(Response => lol(Response))
-  .catch(err => console.error(err))
-
-  function lol(data){
-    let champ = data.data
-            console.log(data)
-            console.log(champ)
-            console.log(data)
-            let ahri = champ.Ahri.image
-            console.log(ahri.full)
-            let ahriImg = ahri.full;
-            let cmpUrl = "http://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/"
-
-            console.log(cmpUrl + ahriImg)
-            let aaa = cmpUrl + ahriImg
-
-            
-
-
-  }
 
 
   return (
-    <main>
+    <BrowserRouter>
+    <header>
+      <Link to='/'>MAIN</Link>
+      <Link to='/ChampData'></Link>
 
+    </header>
+    <main> 
+    <Routes>
+      <Route path="/ChampData" element={<ChampData/>}></Route>
+    </Routes>
     </main>
-
+    </BrowserRouter>
   )
 }
 
---
+++ ProjectLoL/lol/src/component/Jungle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jungle = () => {
+  return (
+    <div>Jungle</div>
+  )
+}
+
+export default Jungle
